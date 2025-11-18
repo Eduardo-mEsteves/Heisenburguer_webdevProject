ab1b6a74a28d1d3e899d88f3a64145f0c4cf8971
Gabriel Furtado Ramalho <emailsopratwitch@gmail.com>
2025-11-18 10:09:22 -0300

    Modificações em "index.php"

---
+++ admin/index.php
@@ -0,0 +1,13 @@
+
+<link rel="stylesheet" href=""> 
+
+<div >
+   
+
+    <nav >
+        <ul>
+            <li><a href="index.php"></a></li>
+            <li><a href=""></a></li>
+        </ul>
+    </nav>
+</div>
