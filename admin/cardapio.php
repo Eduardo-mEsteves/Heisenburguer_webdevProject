d1ba2ffdf9efbe593607a28140e856109076c0e1
Gabriel Furtado Ramalho <emailsopratwitch@gmail.com>
2025-11-24 17:59:35 -0300

    Implementação do CSS no cardapio

--- admin/cardapio.php
+++ admin/cardapio.php
@@ -1,22 +1,27 @@
-<?php
+<link rel="stylesheet" href="heisenburguer.css">
 
+<?php
     require "config.inc.php";
 
-    echo "<p><a href='?pg=cardapio-form'>Adicionar item ao cardapio</a></p>";
+    echo "<p><a class='botao-add' href='?pg=cardapio-form'>Adicionar item ao cardapio</a></p>";
     echo "<h2>Cardápio</h2>";
 
     $sql = "SELECT * FROM receita";
     $resultado = mysqli_query($conexao, $sql);
 
     while ($dados = mysqli_fetch_array($resultado)) {
-    echo "<div>";
-        echo "<p><span>Nome:</span> <strong>" . $dados['nome'] . "</strong></p>";
-        echo "<p><span>Preço:</span> " . $dados['preco'] . "</p>";
-        echo "<blockquote>\"" . $dados['ingredientes'] . "\"</blockquote>";
 
-        echo "<div>";
-            echo "<a href='?pg=item-altera-form&id={$dados['id']}'>Editar</a> <br>";
-            echo "<a href='?pg=item-excluir&id={$dados['id']}'>Excluir</a>";
+        echo "<div class='item'>";
+            echo "<p><strong>Nome:</strong> " . $dados['nome'] . "</p>";
+            echo "<p><strong>Preço:</strong> R$ " . $dados['preco'] . "</p>";
+
+            echo "<blockquote>" . $dados['ingredientes'] . "</blockquote>";
+
+            echo "<div class='acoes'>";
+                echo "<a href='?pg=item-altera-form&id={$dados['id']}'>Editar</a>";
+                echo " | ";
+                echo "<a href='?pg=item-excluir&id={$dados['id']}'>Excluir</a>";
+            echo "</div>";
+
         echo "</div>";
-    echo "</div>";
     }
