edb6c7da698551464f00c37bb8ea4b822515c8df
Eduardo-mEsteves <eduardofrancamesteves@gmail.com>
2025-11-24 18:56:37 -0300

    CSS da pagina de cadastro incorporado

--- admin/cardapio-form.php
+++ admin/cardapio-form.php
@@ -1,17 +1,17 @@
 <link rel="stylesheet" href="heisenburguer.css">
 
-<div>
+<div class="form-box">
     <h2>Cadastrar novo item</h2>
 
     <form action="?pg=item-cadastro" method="post">
         <label for="nome">Nome:</label>
-        <input type="text" name="nome" required><br>
+        <input type="text" name="nome" required>
 
         <label for="preco">Preço:</label>
-        <input type="text" name="preco" required><br>
+        <input type="text" name="preco" required>
 
-        <label for="ingredientes">O que leva:</label><br>
-        <textarea name="ingredientes" rows="5" required></textarea><br>
+        <label for="ingredientes">O que leva:</label>
+        <textarea name="ingredientes" rows="5" required></textarea>
 
         <input type="submit" value="Postar">
     </form>
--- admin/item-cadastro.php
+++ admin/item-cadastro.php
@@ -1,35 +1,37 @@
 <link rel="stylesheet" href="heisenburguer.css">
 
 <?php
-    require_once "config.inc.php";
+require_once "config.inc.php";
 
-    echo "<h1>Cadastrando seu item</h1>";
+echo "<h1>Cadastrando seu item</h1>";
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nome = $_POST["nome"];
-        $preco  = $_POST["preco"];
-        $ingredientes  = $_POST["ingredientes"];
+    $nome = $_POST["nome"];
+    $preco  = $_POST["preco"];
+    $ingredientes  = $_POST["ingredientes"];
 
-        if (!empty($nome) && !empty($preco) && !empty($ingredientes)) {
-            $sql = "INSERT INTO receita (nome, preco, ingredientes)
-                    VALUES ('$nome', '$preco', '$ingredientes')";
-            $inserir = mysqli_query($conexao, $sql);
+    if (!empty($nome) && !empty($preco) && !empty($ingredientes)) {
 
-            if ($inserir) {
-                echo "<h2>Seu item foi adicionado com sucesso!</h2>";
-                echo "<a href='?pg=cardapio'>Voltar</a>";
-            } else {
-                echo "<h2>Erro ao cadastrar item.</h2>";
-                echo mysqli_error($conexao);
-            }
+        $sql = "INSERT INTO receita (nome, preco, ingredientes)
+                VALUES ('$nome', '$preco', '$ingredientes')";
+        $inserir = mysqli_query($conexao, $sql);
+
+        if ($inserir) {
+            echo "<h2 class='sucesso-msg'>Seu item foi adicionado com sucesso!</h2>";
+            echo "<a class='botao-voltar' href='?pg=cardapio'>Voltar</a>";
         } else {
-            echo "<h2>Preencha todos os campos antes de enviar.</h2>";
-            echo "<a href='?pg=cardapio-form'>Voltar ao cadastro do item</a>";
+            echo "<h2>Erro ao cadastrar item.</h2>";
+            echo mysqli_error($conexao);
         }
 
     } else {
-        echo "<h2>Envio de dados não permitido.</h2>";
-        echo "<a href='?pg=cardapio-form'>Voltar ao cadastro do item</a>";
+        echo "<h2>Preencha todos os campos antes de enviar.</h2>";
+        echo "<a class='botao-voltar' href='?pg=cardapio-form'>Voltar ao cadastro do item</a>";
     }
+
+} else {
+    echo "<h2>Envio de dados não permitido.</h2>";
+    echo "<a class='botao-voltar' href='?pg=cardapio-form'>Voltar ao cadastro do item</a>";
+}
 ?>
