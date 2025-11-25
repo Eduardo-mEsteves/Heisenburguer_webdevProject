06ade0af22983e98337dc1b14b4b86c351628be0
Gabriel Furtado Ramalho <emailsopratwitch@gmail.com>
2025-11-25 17:26:50 -0300

    Adicionei textos no index.php

--- admin/index.php
+++ admin/index.php
@@ -28,20 +28,27 @@ if (!isset($_GET['pg']) || $_GET['pg'] == 'inicio') {
 
     <div class="quem-somos">
         <h1>Quem nós somos?</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum dignissimos obcaecati molestias similique est quam pariatur incidunt, dolores accusamus doloremque eius id dicta, optio repellat rem rerum deleniti debitis adipisci! Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus minus reiciendis consectetur omnis delectus pariatur harum nostrum id, unde aliquid, in doloremque tempore voluptate fuga, recusandae beatae iure corrupti vitae. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere molestiae cupiditate inventore ab odio! Eos nam voluptas ipsum provident atque commodi. Ab aperiam quis illo a molestiae dicta aspernatur blanditiis.</p>
+        <p>Somos o Heinsenburger. Nossa filosofia é simples: sem meias medidas.
+
+O Heinsenburger nasceu de uma ideia explosiva: juntar a atmosfera eletrizante de Albuquerque com o hambúrguer mais suculento que você vai provar. Deixamos o trailer de lado e montamos nosso "império" aqui, trazendo receitas que são verdadeiras bombas de sabor.
+Combinamos a precisão científica na escolha dos ingredientes com a arte bruta de fazer um hambúrguer artesanal inesquecível. Aqui, a qualidade não é negociável e o sabor é a nossa única lei.</p>
 
         <div class="blocoA">
             <img src="imagens/epico.jpeg">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, facilis repellat, ab earum doloribus modi necessitatibus delectus laboriosam, nostrum omnis maxime maiores tenetur consequuntur iure nobis pariatur at magni quo.</p>
+            <p>O Heinsenburger nasceu de uma obsessão simples: criar o produto mais puro e viciante da cidade. Nós acreditamos que fazer um hambúrguer perfeito não é apenas cozinhar, é uma ciência exata. Trocamos o trailer no deserto por uma chapa quente e aplicamos a mesma precisão química de Walter White para garantir que cada mordida tenha 99,1% de satisfação garantida. Se você quer qualidade questionável, vá a outro lugar. Se você quer o melhor, você está no lugar certo.</p>
         </div>
 
         <div class="blocoB">
             <img src="imagens/deserto.jpeg">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, facilis repellat, ab earum doloribus modi necessitatibus delectus laboriosam, nostrum omnis maxime maiores tenetur consequuntur iure nobis pariatur at magni quo.</p>
+            <p>LAté quem constrói um império precisa de uma pausa para o almoço.
+
+Nós sabemos que a rotina pode ser pesada, seja negociando territórios ou apenas sobrevivendo à segunda-feira. Por isso, criamos um ambiente onde a única "pressão" é a do queijo derretendo sobre a carne. Nossos sócios e parceiros sabem: quando a fome bate, os negócios esperam. Aqui, o cliente é quem manda e o respeito pela receita é absoluto. Junte sua equipe, estacione o trailer (ou o carro) e venha provar o que realmente importa.</p>
         </div>
 
         <div class="blocoB">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, facilis repellat, ab earum doloribus modi necessitatibus delectus laboriosam, nostrum omnis maxime maiores tenetur consequuntur iure nobis pariatur at magni quo.</p>
+            <p>A Química do Sabor: Respeite a pureza dos ingredientes.
+
+Esqueça o amadorismo. Nossa cozinha é o nosso laboratório. Cada ingrediente é selecionado com rigor científico, desde o blend de carnes até o nosso molho especial "Blue Sky" e os cristais de sabor que são nossa marca registrada. Não usamos atalhos nem misturas baratas. Nós "cozinhamos" com paixão para entregar um produto final cristalino, saboroso e inesquecível. Cuidado: o sabor é altamente viciante e pode causar vontade incontrolável de voltar amanhã.</p>
             <img src="imagens/hamburguer.jpeg">
         </div>
 
